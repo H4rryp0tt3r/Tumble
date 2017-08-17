af2161a9592513197bc4cad0c13acfa0226e9111
Nagesh Podilapu <nagesh.podilapu@thoughtworks.com>
2017-08-17 17:19:33 +0530

    Now our bootloader prints a character to screen :tada:

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,6 +1,20 @@
 	BITS 16			; Start in real mode (only supports 16-bit)
 bootloader:
-	jmp bootloader
+	mov ax, 0x07C0		; 0x07C0 is where BIOS loads bootloader
+	add ax, 288
+	mov ss, ax
+	mov sp, 4096
+
+	mov ax, 0x07C0
+	mov ds, ax
+
+	mov si, our_text	; Put our string position into SI register
+
+	mov ah, 0x0E		; Tell BIOS to print character to TTY
+	lodsb			; Load a single byte of the string at SI register
+	int 0x10		; Call BIOS built-in 'print char' routine
+
+	our_text db 'H', 0	; Our dear string
 
 	times 510-($-$$) db 0   ; Fill remaining space with 0s
 	dw 0xAA55		; Standard bootloader signature
