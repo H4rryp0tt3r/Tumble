8bf5dc658fac7b96483c271b1aca7ea51d090573
Nagesh Podilapu <nagesh.podilapu@thoughtworks.com>
2017-08-17 15:54:42 +0530

    Fix hello world program to exit with status code 0

--- Learnings/Assembly/first_example.asm
+++ Learnings/Assembly/first_example.asm
@@ -1,12 +1,13 @@
 section .text
 	global _start
 _start:
-	mov ecx, len
-	mov edx, message
+	mov edx, len
+	mov ecx, message
 	mov ebx, 1
 	mov eax, 4
 	int 0x80
 
+	mov ebx, 0
 	mov eax, 1
 	int 0x80
 section .data
