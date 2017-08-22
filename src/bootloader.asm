f6c5028673b24fae93aef35b5f8ff84900206214
Nagesh Podilapu <nagesh.podilapu@thoughtworks.com>
2017-08-22 12:10:12 +0530

    Print a string instead of just a character

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,20 +1,28 @@
 	BITS 16			; Start in real mode (only supports 16-bit)
 bootloader:
 	mov ax, 0x07C0		; 0x07C0 is where BIOS loads bootloader
-	add ax, 288
-	mov ss, ax
-	mov sp, 4096
-
-	mov ax, 0x07C0
-	mov ds, ax
+	mov ds, ax		; Start data segment at 0x07C0
+	add ax, 512		; Move 512 bytes from 0x07C0 as our bootloader will occupy that space
+	mov ss, ax		; Start stack segment at ax (i.e 0x07C0 + 512)
+	mov sp, 4096		; Start stack pointer at 4096 (4k stack) (as our stack decreases)
 
 	mov si, our_text	; Put our string position into SI register
+	call print_string
 
+	jmp $			; Keep jumping infinitely! ¯\_(ツ)_/¯
+	our_text db 'Hello!', 0	; Our dear string
+
+print_string:
 	mov ah, 0x0E		; Tell BIOS to print character to TTY
-	lodsb			; Load a single byte of the string at SI register
-	int 0x10		; Call BIOS built-in 'print char' routine
 
-	our_text db 'H', 0	; Our dear string
+repeat:
+	lodsb			; Load a single byte of the string at SI register
+	cmp al, 0
+	je done
+	int 0x10		; Call BIOS built-in 'print char' routine
+	jmp repeat
+done:
+	ret
 
 	times 510-($-$$) db 0   ; Fill remaining space with 0s
 	dw 0xAA55		; Standard bootloader signature
